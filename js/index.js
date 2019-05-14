5202741f245197b84a7829deeca3c821aeddd376
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-14 11:18:11 +0000

    refactor: Use the style on the event's target to set the style effect for the logo

--- js/index.js
+++ js/index.js
@@ -3,9 +3,8 @@ const logoHeading = document.querySelector('.logo-heading');
 const navItems = document.querySelectorAll('nav a');
 
 // Add event listener to flip logo when user clicks on the logo
-logoHeading.addEventListener('click', () => {
+logoHeading.addEventListener('click', (event) => {
   // console.log(event)
-  logoHeading.setAttribute('style', '-moz-transform: scale(-1, 1); -webkit-transform: scale(-1, 1);' +
-    '-o-transform: scale(-1, 1); -ms-transform: scale(-1, 1); transform: scale(-1, 1);'
-  )
+  event.target.style = '-moz-transform: scale(-1, 1); -webkit-transform: scale(-1, 1);' +
+  '-o-transform: scale(-1, 1); -ms-transform: scale(-1, 1); transform: scale(-1, 1);';
 });

e691e2b13c5a8dc6b1ccdc919df0acdf5b958288
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-14 11:21:36 +0000

    feature: Add event listener to reset logo to default when user moves mouse out

--- js/index.js
+++ js/index.js
@@ -8,3 +8,9 @@ logoHeading.addEventListener('click', (event) => {
   event.target.style = '-moz-transform: scale(-1, 1); -webkit-transform: scale(-1, 1);' +
   '-o-transform: scale(-1, 1); -ms-transform: scale(-1, 1); transform: scale(-1, 1);';
 });
+
+// Add event listener to reset logo to default when user moves mouse out
+logoHeading.addEventListener('mouseout', (event) => {
+  event.target.style = '-moz-transform: ""; -webkit-transform: "";' +
+  '-o-transform: ""; -ms-transform: ""; transform: "";';
+})

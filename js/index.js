dd5889ed7e21d992493efe65b67b705604c9f39e
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-14 11:13:25 +0000

    feature: Add event listener to flip logo when user clicks on the logo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const logoHeading = document.querySelector('.logo-heading');
+const navItems = document.querySelectorAll('nav a');
+
+// Add event listener to flip logo when user clicks on the logo
+logoHeading.addEventListener('click', () => {
+  // console.log(event)
+  logoHeading.setAttribute('style', '-moz-transform: scale(-1, 1); -webkit-transform: scale(-1, 1);' +
+    '-o-transform: scale(-1, 1); -ms-transform: scale(-1, 1); transform: scale(-1, 1);'
+  )
+});
